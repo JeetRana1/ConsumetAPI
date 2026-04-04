a3ef8039e132bc26bf751520ccf8ce4f41955926
JeetRana1 <104169225+JeetRana1@users.noreply.github.com>
2026-04-04 09:36:03 -0400

    fix(proxy): match VM folder name (ConsumetAPI)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'consumet-api',
     script: 'dist/main.js',
-    cwd: process.env.HOME + '/api.consumet.org',
+    cwd: process.env.HOME + '/ConsumetAPI',
     interpreter: process.env.HOME + '/.bun/bin/bun',
     cron_restart: '0 0 * * *',
     env: {
